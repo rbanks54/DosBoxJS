939ae38f5aa4a0c3d59dd5ab42522e8cd202e207
Richard Banks <rbanks54@msn.com>
2014-01-29 19:20:27 +1100

    working on adding directories and file support to the drive object

--- PureJSWebSite/Scripts/PsuedoDrive.js
+++ PureJSWebSite/Scripts/PsuedoDrive.js
@@ -2,16 +2,40 @@
 var psuedoDrive = (function () {
     var module = {};
 
-    module.directory = function(name) {
-        this.Name = name;
-    };
-
     module.drive = function (driveName) {
         var driveLetter = driveName.charAt(0).toUpperCase() + ':';
         this.DriveLetter = driveLetter;
         this.RootDirectory = new module.directory(driveLetter);
         this.CurrentDirectory = this.RootDirectory;
     };
-    
+
+    module.directory = function (name) {
+        this.Name = name;
+        this.files = [];
+        this.subDirectories = [];
+    };
+
+    module.directory.prototype.addDirectory = function (subDirName) {
+        var subDir = new module.directory(subDirName);
+        this.subDirectories.join(subDir);
+        return subDir;
+    };
+
+    module.directory.prototype.addFile = function (fileName, content) {
+        var f = new file(fileName, content);
+        this.files.join(f);
+        return f;
+    };
+
+    module.directory.prototype.getItemFromPath = function (path) {
+        return undefined;
+    };
+
+
+    var file = function (fileName, content) {
+        this.FileName = fileName;
+        this.Content = content;
+    };
+
     return module;
 })();
--- Tests/DriveTests/DriveTests.js
+++ Tests/DriveTests/DriveTests.js
@@ -2,8 +2,23 @@
 /// <reference path="../../purejswebsite/scripts/psuedodrive.js" />
 
 describe('drive tests', function () {
-    it('should create a new drive', function() {
-        var driveName = "d";
+    var drive;
+
+    beforeEach(function () {
+        drive = new psuedoDrive.drive('C');
+        var rootDir = drive.RootDirectory;
+        rootDir.addFile('FileInRoot1', '');
+        rootDir.addFile('FileInRoot2', '');
+
+        var subDir1 = rootDir.addDirectory('subDir1');
+        subDir1.addFile('File1InDir1', '');
+        subDir1.addFile('File2InDir1', '');
+
+        rootDir.addDirectory('subDir2');
+    });
+
+    it('should create a new drive', function () {
+        var driveName = 'd';
 
         var testdrive = new psuedoDrive.drive(driveName);
         expect(testdrive.RootDirectory).toEqual(testdrive.CurrentDirectory);
@@ -11,7 +26,15 @@ describe('drive tests', function () {
         expect(testdrive.RootDirectory.Name).toBe('D:');
         expect(testdrive.DriveLetter).toBe('D:');
 
-        testdrive = new psuedoDrive.drive("Hello");
+        testdrive = new psuedoDrive.drive('Hello');
         expect(testdrive.DriveLetter).toBe('H:');
+    });
+
+    it('should be able to change directory', function () {
+        expect(drive.CurrentDirectory.Name).toBe('C:');
+
+        var subDir = drive.ChangeCurrentDirectory('subDir1');
+        expect(drive.CurrentDirectory.Name).toBe('subDir1');
+        expect(subDir.Name).toBe('subDir1');
     });
 });
