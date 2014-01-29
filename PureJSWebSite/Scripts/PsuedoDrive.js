6b48d6b945d6b308e953b6039ccba541c85ea1f8
Richard Banks <rbanks54@msn.com>
2014-01-29 18:59:17 +1100

    adding some tests and starting on the fake drive implementation

---
+++ PureJSWebSite/Scripts/PsuedoDrive.js
@@ -0,0 +1,17 @@
+﻿// Code for our fake disk drive and file system that we're using
+var psuedoDrive = (function () {
+    var module = {};
+
+    module.directory = function(name) {
+        this.Name = name;
+    };
+
+    module.drive = function (driveName) {
+        var driveLetter = driveName.charAt(0).toUpperCase() + ':';
+        this.DriveLetter = driveLetter;
+        this.RootDirectory = new module.directory(driveLetter);
+        this.CurrentDirectory = this.RootDirectory;
+    };
+    
+    return module;
+})();
---
+++ Tests/DriveTests/DriveTests.js
@@ -0,0 +1,17 @@
+﻿/// <reference path="../Scripts/jasmine.js" />
+/// <reference path="../../purejswebsite/scripts/psuedodrive.js" />
+
+describe('drive tests', function () {
+    it('should create a new drive', function() {
+        var driveName = "d";
+
+        var testdrive = new psuedoDrive.drive(driveName);
+        expect(testdrive.RootDirectory).toEqual(testdrive.CurrentDirectory);
+        expect(testdrive.CurrentDirectory.Name).toBe('D:');
+        expect(testdrive.RootDirectory.Name).toBe('D:');
+        expect(testdrive.DriveLetter).toBe('D:');
+
+        testdrive = new psuedoDrive.drive("Hello");
+        expect(testdrive.DriveLetter).toBe('H:');
+    });
+});
